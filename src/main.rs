e33418cdb833cad9af0c9140d2e85fef7c279b3c
Ashraf_Hossain <ashraf7rubby@gmail.com>
2022-08-19 23:47:20 +0600

    access keyword added

--- src/main.rs
+++ src/main.rs
@@ -41,9 +41,17 @@ fn main() {
 
     
     let mut generator = |line: Vec<String>| {
+        let accessKeywords: HashMap<String,i8> = HashMap::from([
+            ("public".to_string(), 1),
+            ("private".to_string(),1),
+            ("protected".to_string(),1),
+            ("static".to_string(), 1)
+        ]);
         let mut currentLine = String::from("");
         let mut lastWord = String::from("");
+        let mut nextWord = String::from("");
         let mut quotes: i8 = 0;
+        // let accessKeywords = vec!["public","private","protected","static"];
         for i in 0..line.len(){
             let currentWord = &line[i];
             if currentWord == "\""{
@@ -53,27 +61,45 @@ fn main() {
             if currentWord == " " || quotes == 1{
                 continue;
             }
+            if i + 1 < line.len(){
+                nextWord = line[i + 1].clone();
+            }
             if currentWord == "=" {
-                if quotes == 0 {
+                if quotes == 0 &&  nextWord != ">"{
                     is_variable.insert(lastWord , true);
                     lastWord = "".to_string();
                 }
+                continue;
+            }
+            if accessKeywords.contains_key(&lastWord){
+                is_variable.insert(currentWord.to_string(), true);
+                lastWord = "".to_string();
             }else{
                 lastWord = (&currentWord).to_string();
             }
         }
+        quotes = 0;
         for i in 0..line.len(){
             let mut currentWord = line[i].clone();
-            if currentWord == " "{
+            if currentWord == " " && quotes == 1{
                 currentLine.push(' ');
                 continue;
             }
+            if currentWord == "\""{
+                quotes += 1;
+                quotes %= 2;
+                currentLine.push_str(&currentWord);
+                continue;
+            }
             if is_variable.contains_key(&currentWord){
-                currentWord.insert(0,'$');
+                if quotes == 0{
+                    currentWord.insert(0,'$');
+                }
                 currentLine.push_str(&currentWord);
             }else{
                 currentLine.push_str(&currentWord);
             }
+            lastWord = (&currentWord).to_string();
         }
         currentLine = currentLine.replace("{{" , "<?php ");
         currentLine = currentLine.replace("}}" , "?>");
